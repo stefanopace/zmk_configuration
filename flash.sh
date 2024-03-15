9c2ae9586ce2848129e0db90ed45240a39ba0b43
Stefano Pace <space@coders51.com>
2024-03-15 16:45:04 +0100

    fixed

--- flash.sh
+++ flash.sh
@@ -3,6 +3,8 @@
 container_name=$(docker ps | grep -o "^[a-z0-9]*")
 echo $container_name
 
+# docker volume create --driver local -o o=bind -o type=none -o device="/home/stefano/fun/keyboards/zmk_configuration/" zmk-config
+
 docker exec -it --workdir /workspaces/zmk/app $container_name west build --pristine -b nice_nano_v2 -- -DSHIELD=contra_rgb -DZMK_CONFIG="/workspaces/zmk-config/config"
 
 if [[ $? -ne 0 ]]; then echo "fail"; exit 1 ;fi
@@ -16,4 +18,5 @@ done
 
 echo ""
 
-cp "/home/stefano/work-in-progress/zmk/app/build/zephyr/zmk.uf2" "/media/stefano/NICENANO"
+cp "/home/stefano/fun/keyboards/zmk_firmware/zmk/app/build/zephyr/zmk.uf2" "/media/stefano/NICENANO"
+
