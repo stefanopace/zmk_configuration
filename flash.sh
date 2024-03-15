a8ca2e7ecc2d7c079dde43868cdfea5a69cc6f69
Stefano Pace <space@coders51.com>
2024-03-15 16:54:40 +0100

    adds caps word

--- flash.sh
+++ flash.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+# seguire le istruzioni per docker (https://zmk.dev/docs/development/setup)
+# docker volume create --driver local -o o=bind -o type=none -o device="/home/stefano/fun/keyboards/zmk_configuration/" zmk-config
+
 container_name=$(docker ps | grep -o "^[a-z0-9]*")
 echo $container_name
-
-# docker volume create --driver local -o o=bind -o type=none -o device="/home/stefano/fun/keyboards/zmk_configuration/" zmk-config
 
 docker exec -it --workdir /workspaces/zmk/app $container_name west build --pristine -b nice_nano_v2 -- -DSHIELD=contra_rgb -DZMK_CONFIG="/workspaces/zmk-config/config"
 
